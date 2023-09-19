e398276e849e6a4592dc86da9bc128e2cc353743
iwdath <lazorgrechak@gmail.com>
2023-09-19 11:56:41 +0300

    added picture changer and smoothly lib

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
 let phrases = [
-  'отправить другу смешную гифку',
-  'посмотреть скидки на авиабилеты',
-  'разобраться, о чём поют рэперы',
-  'Юрий Дудь',
-  'расставить книги на полке по цвету',
-  'читать про зарплаты в Сан-Франциско'
+	{ text: 'попасть в поток грустных песен и вспомнить все ошибки молодости', image: 'https://code.s3.yandex.net/web-code/procrastinate/8.png' },
+	{ text: 'посмотреть трейлер сериала и заодно первый сезон', image: 'https://code.s3.yandex.net/web-code/procrastinate/9.png' },
+	{ text: 'прочитать новости и ужаснуться в комментариях', image: 'https://code.s3.yandex.net/web-code/procrastinate/7.png' },
+	{ text: 'проверить непрочитанное в Telegram-каналах', image: 'https://code.s3.yandex.net/web-code/procrastinate/10.png' },
+	{ text: 'читать про зарплаты в Сан-Франциско', image: 'https://code.s3.yandex.net/web-code/procrastinate/6.png' },
+	{ text: 'расставить книги на полке по цвету', image: 'https://code.s3.yandex.net/web-code/procrastinate/5.png' },
+	{ text: 'посмотреть скидки на авиабилеты', image: 'https://code.s3.yandex.net/web-code/procrastinate/2.png' },
+	{ text: 'разобраться, о чём поют рэперы', image: 'https://code.s3.yandex.net/web-code/procrastinate/3.png' },
+	{ text: 'отправить другу смешную гифку', image: 'https://code.s3.yandex.net/web-code/procrastinate/1.gif' },
+	{ text: 'Юрий Дудь', image: 'https://code.s3.yandex.net/web-code/procrastinate/4.png' }
 ];
 
 function getRandomElement(arr) {
-  let randIndex = Math.floor(Math.random() * arr.length);
-  return arr[randIndex];
+	let randIndex = Math.floor(Math.random() * arr.length);
+	return arr[randIndex];
 }
 
 let button = document.querySelector('.button');
@@ -18,5 +22,13 @@ let advice = document.querySelector('.advice');
 let image = document.querySelector('.image');
 
 button.addEventListener('click', function () {
-  phrase.textContent = getRandomElement(phrases);
+	let randomElement = getRandomElement(phrases);
+	smoothly(phrase, 'textContent', randomElement.text);
+	smoothly(image, 'src', randomElement.image);
+
+	if (randomElement.text.length > 40) {
+		advice.style.fontSize = '33px';
+	} else {
+		advice.style.fontSize = '42px';
+	}
 });
